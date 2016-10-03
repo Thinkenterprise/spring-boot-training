830864a0885f72f7688ac86a2d06e4d3d72eb31f
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-03 20:00:36 +0200

    Implemented RouteControllerAdvice.

---
+++ spring-boot-rest/src/main/java/com/thinkenterprise/controller/RouteControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.thinkenterprise.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class RouteControllerAdvice {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String exception(Exception exception) {
+        return exception.getMessage();
+    }
+}
