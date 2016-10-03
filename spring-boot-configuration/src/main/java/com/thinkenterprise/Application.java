cfcd3bf490afe76ede84a3f119888d599af35e75
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-03 14:05:35 +0200

    Removed unused @Autowire Annotation in Application.java.

--- spring-boot-configuration/src/main/java/com/thinkenterprise/Application.java
+++ spring-boot-configuration/src/main/java/com/thinkenterprise/Application.java
@@ -2,7 +2,6 @@ package com.thinkenterprise;
 
 
 import com.thinkenterprise.service.RouteService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
