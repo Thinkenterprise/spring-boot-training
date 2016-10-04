8e1bfbd36641cc40a4a41c1411d5a1ebf4508aae
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-04 16:19:26 +0200

    Implemented RouteRepositoryImplTest.

---
+++ spring-boot-data/src/test/java/com/thinkenterprise/repository/RouteRepositoryImplTest.java
@@ -0,0 +1,29 @@
+package com.thinkenterprise.repository;
+
+import com.thinkenterprise.domain.route.Route;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RouteRepositoryImplTest {
+
+    @Autowired
+    private RouteRepository sut;
+
+    @Test
+    public void findByDeparture() throws Exception {
+        Iterable<Route> actual = sut.findByDeparture("FRA");
+        Assert.assertEquals("FRA", actual.iterator().next().getDeparture());
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        Iterable<Route> actual = sut.findAll();
+        Assert.assertNotNull(actual.iterator().hasNext());
+    }
+}
