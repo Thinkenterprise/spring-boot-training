79cdcf1ac63fe6cfb88b4d798cae63d8ec42dc3d
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-05 18:16:20 +0200

    Implemented missing finder methods in RouteRepository.

--- spring-boot-data/src/main/java/com/thinkenterprise/repository/RouteRepository.java
+++ spring-boot-data/src/main/java/com/thinkenterprise/repository/RouteRepository.java
@@ -8,6 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface RouteRepository extends CrudRepository<Route, Long> {
 
+    Iterable<Route> findByFlightNumber(@Param("flightNumber") String flightNumber);
+
     @Query("select r from Route r where r.departure = :departure")
     Iterable<Route> findByDeparture(@Param("departure") String departure);
+
+    @Query(value = "select * from Route r where r.destination = :destination", nativeQuery = true)
+    Iterable<Route> findByDestination(@Param("destination") String destination);
+
 }
