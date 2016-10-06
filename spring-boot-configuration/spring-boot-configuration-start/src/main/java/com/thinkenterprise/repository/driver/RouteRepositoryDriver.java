c37b04f17bddfd04f719cd9afd1e7881b24ba0d6
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-06 18:20:30 +0200

    Implemented spring-boot-configuration-start.

--- spring-boot-configuration/spring-boot-configuration-start/src/main/java/com/thinkenterprise/repository/driver/RouteRepositoryDriver.java
+++ spring-boot-configuration/spring-boot-configuration-start/src/main/java/com/thinkenterprise/repository/driver/RouteRepositoryDriver.java
@@ -1,10 +1,4 @@
 package com.thinkenterprise.repository.driver;
 
-import com.thinkenterprise.domain.route.Route;
-
-import java.util.List;
-
 public interface RouteRepositoryDriver {
-
-    List<Route> getRouteList();
 }
