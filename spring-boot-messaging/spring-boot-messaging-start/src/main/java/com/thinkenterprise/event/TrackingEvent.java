d39af85dc820c8a067991b8ae5a0e87ada4abfaa
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-11 20:23:08 +0200

    TrackingEvent, TrackingEventListener and TrackingEventPublisher are provided in spring-boot-messaging-start.

--- spring-boot-messaging/spring-boot-messaging-start/src/main/java/com/thinkenterprise/event/TrackingEvent.java
+++ spring-boot-messaging/spring-boot-messaging-start/src/main/java/com/thinkenterprise/event/TrackingEvent.java
@@ -1,4 +1,20 @@
 package com.thinkenterprise.event;
 
+import com.thinkenterprise.domain.tracking.Tracking;
+
 public class TrackingEvent {
+
+    private Tracking tracking;
+
+    public TrackingEvent(Tracking tracking) {
+        this.tracking = tracking;
+    }
+
+    public Tracking getTracking() {
+        return tracking;
+    }
+
+    public void setTracking(Tracking tracking) {
+        this.tracking = tracking;
+    }
 }
--- spring-boot-messaging/spring-boot-messaging-start/src/main/java/com/thinkenterprise/event/TrackingEventPublisher.java
+++ spring-boot-messaging/spring-boot-messaging-start/src/main/java/com/thinkenterprise/event/TrackingEventPublisher.java
@@ -1,4 +1,17 @@
 package com.thinkenterprise.event;
 
+import com.thinkenterprise.domain.tracking.Tracking;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TrackingEventPublisher {
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    public void publishTracking(Tracking tracking) {
+        this.publisher.publishEvent(new TrackingEvent(tracking));
+    }
 }
