f5a60e9c12412ceb8e1a4a3d1bfa4c9c761497cd
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-16 16:12:50 +0200

    Implemented spring-boot-data-final UseCase.

---
+++ spring-boot-data/spring-boot-data-final/src/main/java/com/thinkenterprise/repository/FlightRepository.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 Thinkenterprise
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @author Rafael Kansy
+ * @author Michael Schaefer
+ */
+
+package com.thinkenterprise.repository;
+
+import com.thinkenterprise.domain.route.Flight;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import java.time.LocalDate;
+
+
+public interface FlightRepository extends CrudRepository<Flight, Long> {
+
+    @Query("select f from Flight f where f.date = :date")
+    Iterable<Flight> findByDate(@DateTimeFormat(iso = ISO.DATE) @Param("date") LocalDate date);
+
+}
---
+++ spring-boot-data/spring-boot-data-final/src/main/java/com/thinkenterprise/repository/RouteRepository.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 Thinkenterprise
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @author Rafael Kansy
+ * @author Michael Schaefer
+ */
+
+package com.thinkenterprise.repository;
+
+import com.thinkenterprise.domain.route.Route;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+
+public interface RouteRepository extends CrudRepository<Route, Long> {
+    Iterable<Route> findByDeparture(@Param("departure") String departure);
+
+    @EntityGraph(value = "routeFlight", type = EntityGraph.EntityGraphType.LOAD)
+    Iterable<Route> getByDestination(@Param("destination") String destination);
+
+    @Query(value = "select r from Route r where r.departure = :departure")
+    Iterable<Route> queryFindByDeparture(@Param("departure") String departure);
+
+    @Query(value = "select r from Route r where r.destination = :destination")
+    Iterable<Route> findByDestination(@Param("destination") String destination);
+
+    @Query(value = "SELECT * FROM ROUTE WHERE FLIGHT_NUMBER = ?1", nativeQuery = true)
+    Iterable<Route> findByFlightNumber(@Param("flightNumber") String flightNumber);
+}
