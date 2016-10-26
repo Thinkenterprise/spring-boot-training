f1c588524d75e1c9db8c49a1a55caf8e8b78ea28
Michael Schaefer <Michael.Schaefer@msg-systems.com>
2016-10-26 07:32:03 +0200

    Der Service muss bereits implementiert sein. Es steht in der Übung nur, dass der Web Controller implementiert werden soll. Es gibt keine Rest Controller. Die Klassen gab es schon. Zwar nicht implementiert, trotzdem keine Klassen. Auch den Test komplett raus.

--- spring-boot-rest/spring-boot-rest-start/src/main/java/com/thinkenterprise/Application.java
+++ spring-boot-rest/spring-boot-rest-start/src/main/java/com/thinkenterprise/Application.java
@@ -32,7 +32,6 @@ public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         RouteService routeService = context.getBean(RouteService.class);
-        routeService.findAll();
-        ;
+        
     }
 }
--- spring-boot-rest/spring-boot-rest-start/src/main/java/com/thinkenterprise/service/RouteService.java
+++ spring-boot-rest/spring-boot-rest-start/src/main/java/com/thinkenterprise/service/RouteService.java
@@ -20,5 +20,58 @@
 
 package com.thinkenterprise.service;
 
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thinkenterprise.domain.route.Route;
+import com.thinkenterprise.repository.RouteRepository;
+
+
+@Service
 public class RouteService {
+    @Autowired
+    private RouteRepository routeRepository;
+
+    public int calculateFlightCount(Route route) {
+        return route.getFlights().size();
+    }
+
+    public String formatTime(LocalTime time) {
+        return time.toString();
+    }
+
+    public Iterable<Route> findAll() {
+        return routeRepository.findAll();
+    }
+
+    public Iterable<Route> findByDeparture(String departure) {
+        return routeRepository.findByDeparture(departure);
+    }
+
+    public boolean exists(long id)  {
+        boolean result = routeRepository.exists(id);
+        if (!result) {
+        	return result;
+        } else {
+            return result;
+        }
+    }
+
+    public long count() {
+        return routeRepository.count();
+    }
+
+    public void delete(long id) {
+        routeRepository.delete(id);
+    }
+
+    public Route save(Route entity) {
+        return routeRepository.save(entity);
+    }
+
+    public Route findById(long id) {
+        return routeRepository.findOne(id);
+    }
 }
