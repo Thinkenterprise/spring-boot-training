96741bb94532b20684e650b9c3e7459bfb0463ea
Rafael Kansy <rafael.kansy@blue-sharp.de>
2016-10-27 18:25:02 +0200

    RouteController should return ResponseEntity not of the entity.

--- spring-boot-rest/spring-boot-rest-basic/src/main/java/com/thinkenterprise/controller/RouteController.java
+++ spring-boot-rest/spring-boot-rest-basic/src/main/java/com/thinkenterprise/controller/RouteController.java
@@ -23,6 +23,7 @@ package com.thinkenterprise.controller;
 import com.thinkenterprise.domain.route.Route;
 import com.thinkenterprise.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -41,42 +42,43 @@ public class RouteController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id) throws RouteNotFoundException {
+    public ResponseEntity delete(@PathVariable(value = "id") Long id) throws RouteNotFoundException {
         try {
             service.exists(id);
             service.delete(id);
+            return ResponseEntity.accepted().build();
         } catch (RouteNotFoundException e) {
             throw e;
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Route get(@PathVariable(value = "id") Long id) throws RouteNotFoundException {
+    public ResponseEntity<Route> get(@PathVariable(value = "id") Long id) throws RouteNotFoundException {
         try {
             service.exists(id);
-            return service.findById(id);
+            return ResponseEntity.ok(service.findById(id));
         } catch (RouteNotFoundException e) {
             throw e;
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public Route put(@Valid @RequestBody Route entity) {
-        return service.save(entity);
+    public ResponseEntity<Route> put(@Valid @RequestBody Route entity) {
+        return ResponseEntity.ok(service.save(entity));
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.POST)
-    public Route post(@Valid @RequestBody Route entity) {
-        return service.save(entity);
+    public ResponseEntity<Route> post(@Valid @RequestBody Route entity) {
+        return ResponseEntity.ok(service.save(entity));
     }
 
     @RequestMapping("/")
-    public Iterable<Route> findAll() {
-        return service.findAll();
+    public ResponseEntity<Iterable<Route>> findAll() {
+        return ResponseEntity.ok(service.findAll());
     }
 
     @RequestMapping("/search/findByDeparture")
-    public Iterable<Route> findByDeparture(@RequestParam(value = "departure") String departure) {
-        return service.findByDeparture(departure);
+    public ResponseEntity<Iterable<Route>> findByDeparture(@RequestParam(value = "departure") String departure) {
+        return ResponseEntity.ok(service.findByDeparture(departure));
     }
 }
