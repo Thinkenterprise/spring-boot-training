38b86eb22da2c346ecc83a4f8a7e3a93c07606e3
thinkenterprise <michael.schaefer@msg-systems.com>
2018-09-15 22:38:16 +0200

    introduce HTML Controller

--- spring-boot-basics/spring-boot-basics-basic/src/main/java/com/thinkenterprise/controller/HelloWorldController.java
+++ spring-boot-basics/spring-boot-basics-basic/src/main/java/com/thinkenterprise/controller/HelloWorldController.java
@@ -20,14 +20,14 @@
 
 package com.thinkenterprise.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
-    @RequestMapping
+	@RequestMapping
     public String index() {
-        return "Hello World";
+        return "index";
     }
 }
