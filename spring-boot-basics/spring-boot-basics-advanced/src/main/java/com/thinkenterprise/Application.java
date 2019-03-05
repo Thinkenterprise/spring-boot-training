bba23b9e462b444e98e9bd0fa021c20256ed8b07
Thinkenterprise <michael.schaefer@msg-systems.com>
2019-03-05 12:56:48 +0100

    Maven Update to 2.0

--- spring-boot-basics/spring-boot-basics-advanced/src/main/java/com/thinkenterprise/Application.java
+++ spring-boot-basics/spring-boot-basics-advanced/src/main/java/com/thinkenterprise/Application.java
@@ -24,13 +24,13 @@ package com.thinkenterprise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args); 
     }
 
     @Override
